fcb10a36e2ee514dfc77bf42152fbc47b2cdb845
Yogesh <yogeshsaini23042000@gmail.com>
2021-12-29 21:49:24 +0530

    Success!!

--- lib/main.dart
+++ lib/main.dart
@@ -27,11 +27,40 @@ class _MyAppState extends State<MyApp> {
 
   void runAi() {
     var input = [
-      [1.23, 6.54, 7.81, 3.21, 2.22]
+      -1.82313293,
+      1.38265717,
+      -4.77082405,
+      2.56861916,
+      2.11227188,
+      2.15514374,
+      2.49683642,
+      1.80431137,
+      -1.14440718,
+      1.61954746,
+      2.83308411,
+      1.60585175,
+      -1.42026205,
+      1.26343222,
+      5.66473305,
+      -2.95916677,
+      -5.26943840,
+      1.46824141,
+      5.01206589,
+      1.31672490,
+      -5.25899696,
+      2.26965380,
+      2.02486858,
+      7.88749838,
+      -2.70444465,
+      -1.77783465,
+      -5.90589285,
+      1.15026522,
+      -1.21936393,
+      -9.79906559,
     ];
 
     // if output tensor shape [1,2] and type is float32
-    var output = List.filled(1 * 2, 0).reshape([1, 2]);
+    var output = List.filled(1 * 9, 0).reshape([1, 9]);
 
     // inference
     interpreter.run(input, output);
@@ -42,6 +71,11 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    runAi();
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Done"),
+      ),
+    );
   }
 }
